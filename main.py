ae86a80c704129ad32475d4d66106e5f0239e473
daniel1-2 <daniel263k@gmail.com>
2024-08-09 22:50:40 -0500

    done, change the open CV text

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ shoulder_threshold = 0
 neck_threshold = 0
 
 # alert/status vars
+ready_to_start = False
 alert = 'alert.mp3' # your alert audio
 sit = True
 sedentary = False
@@ -78,108 +79,116 @@ def not_sitting(time):
 # Initialize webcam
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
-    keyInput = cv2.waitKey(10)
+    keyInput = cv2.waitKey(1)
     ret, frame = cap.read()
 
     if not ret:
         print("Failed to grab frame")
         break
+    
+    if not ready_to_start:
+        cv2.putText(frame, "Please Sit Straight. When Ready Hit Space To Launch", (50,50), font, 1, (0, 0, 0), 2, cv2.LINE_4)
+        if keyInput == ord(' '):
+            ready_to_start = True
+    
+    elif ready_to_start:    
+    
+        if setup_frames < 50: 
+            cv2.putText(frame, "Analyzing your good posture", (50,50), font, 1, (0, 0, 0), 2, cv2.LINE_4)
 
-    if setup_frames < 25: 
-        cv2.putText(frame, "Analyzing your good posture", (50,50), font, 1, (0, 0, 0), 2, cv2.LINE_4)
+        # Convert BGR image to RGB
+        rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Convert BGR image to RGB
-    rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Process frames with Mediapipe
+        pose_results = pose.process(rgb_frame)
+        # object_results = object_detection.process(rgb_frame)
 
-    # Process frames with Mediapipe
-    pose_results = pose.process(rgb_frame)
-    # object_results = object_detection.process(rgb_frame)
+        # Draw pose landmarks
+        #detects if landmarks are visible
 
-    # Draw pose landmarks
-    #detects if landmarks are visible
+        if pose_results.pose_landmarks:
+            mp_drawing.draw_landmarks(
+                frame, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS
+                )
+            
+            #if all landmarks detected for more than set time, you are asked to take a break
+            #there's a snooze option that will reset the timer
+            time_diff = time.time() - start_time 
+            if time_diff > 30:
+                cv2.putText(frame, 
+                f"It has been {(time_diff/60):.1f} minutes, Please Take A Break",
+                (70,80), font, 1, (0,0,0), 2, cv2.LINE_AA)
+                cv2.putText(frame, "Please Press Esc To Reset",
+                (270,280), font, 1, (0,0,0), 2, cv2.LINE_AA)
+                if keyInput == 27:
+                    start_time = time.time()
 
-    if pose_results.pose_landmarks:
-        mp_drawing.draw_landmarks(
-            frame, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS
-            )
+            landmarks = pose_results.pose_landmarks.landmark
+
+            # Get key body parts
+            left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x * frame.shape[1],
+                            landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y * frame.shape[0]]
+            right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x * frame.shape[1]
+                            ,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y * frame.shape[0]]
+            nose = [landmarks[mp_pose.PoseLandmark.NOSE.value].x * frame.shape[1],
+                    landmarks[mp_pose.PoseLandmark.NOSE.value].y * frame.shape[0]]
         
-        #if all landmarks detected for more than set time, you are asked to take a break
-        #there's a snooze option that will reset the timer
-        time_diff = time.time() - start_time 
-        if time_diff > 30:
-            cv2.putText(frame, 
-            f"It has been {(time_diff/60):.1f} minutes, Please Take A Break",
-            (70,80), font, 1, (0,0,0), 2, cv2.LINE_AA)
-            cv2.putText(frame, "Please Press Esc To Reset",
-            (270,280), font, 1, (0,0,0), 2, cv2.LINE_AA)
-            if keyInput == 27:
+            # Calculate mid point of shoulders
+            mid_shoulder = [(left_shoulder[0] + right_shoulder[0]) / 2, (left_shoulder[1] + right_shoulder[1]) / 2]
+            
+            # Calculate angles
+            shoulder_angle = 180 - calculate_angle(left_shoulder, nose, right_shoulder)
+            neck_angle = calculate_angle(mid_shoulder, nose, [nose[0], 0])
+            # shoulder_angle = calculate_angle(left_shoulder, nose, right_shoulder)
+            # neck_angle = calculate_angle(mid_shoulder, nose, [nose[0], mid_shoulder[1]])
+
+            # setting up baseline angles from first 25 frames
+            if not posture_setup_complete and setup_frames < 50:
+                initial_shoulder_angles.append(shoulder_angle)
+                initial_neck_angles.append(neck_angle)
+                setup_frames += 1
+                cv2.putText(frame, f"Gathering data... {setup_frames}/50", 
+                            (10, 30), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
+            
+            # after 50 frames, take the average angles and set thresholds for angles
+            ## TODO, MAKE TEXT DISPLAY LONGER 
+            elif not posture_setup_complete:
+                shoulder_threshold = np.mean(initial_shoulder_angles) - 10
+                neck_threshold = np.mean(initial_neck_angles) - 10
+                os.system('clear')           
+                posture_setup_complete = True
+                cv2.putText(frame, 
+                f"Setup complete! Shoulder threshold: {shoulder_threshold:.1f} and neck threshold: {neck_threshold:.1f}",
+                (70,80), font, 1, (0,0,0), 2, cv2.LINE_AA)
+      
+                #print(f"Setup complete! Shoulder threshold: {shoulder_threshold:.1f} and neck threshold: {neck_threshold:.1f}")
+
+            # Begin posture detection
+            elif posture_setup_complete:
+                # poor shoulder posture
+                if shoulder_threshold > shoulder_angle and neck_angle > neck_threshold:
+                    cv2.putText(frame,
+                    f"Poor shoulder posture detected! Please sit up straight. {shoulder_angle:.1f}/{shoulder_threshold:.1f}",
+                    (100,50), font, 1, (255,0,0), 2, cv2.LINE_4)
+                    poor_posture_detected()
+                
+                # poor neck posture
+                if neck_threshold > neck_angle and shoulder_angle > shoulder_threshold:
+                    cv2.putText(frame,
+                    f"Poor neck posture detected! Please sit up straight. {neck_angle:.1f}/{neck_threshold:.1f}",
+                    (100,50), font, 1, (255,0,0), 2, cv2.LINE_4)
+                    poor_posture_detected()
+
+                if shoulder_threshold > shoulder_angle and neck_threshold > neck_angle:
+                    cv2.putText(frame,
+                    f"""Poor neck and shoulder posture detected! Please sit up straight. Shoulder: {shoulder_angle:.1f}/{shoulder_threshold:.1f} Neck: {neck_angle:.1f}/{neck_threshold:.1f}""",
+                    (100,50), font, 1, (255,0,0), 2, cv2.LINE_4)
+                    poor_posture_detected()
+                print (nose, left_shoulder, right_shoulder)
+                
+        else: 
+            if (time.time() - (start_time + time_diff)) > 15:
                 start_time = time.time()
-
-        landmarks = pose_results.pose_landmarks.landmark
-
-        # Get key body parts
-        left_shoulder = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x * frame.shape[1],
-                         landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y * frame.shape[0]]
-        right_shoulder = [landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].x * frame.shape[1]
-                          ,landmarks[mp_pose.PoseLandmark.RIGHT_SHOULDER.value].y * frame.shape[0]]
-        nose = [landmarks[mp_pose.PoseLandmark.NOSE.value].x * frame.shape[1],
-                landmarks[mp_pose.PoseLandmark.NOSE.value].y * frame.shape[0]]
-       
-        # Calculate mid point of shoulders
-        mid_shoulder = [(left_shoulder[0] + right_shoulder[0]) / 2, (left_shoulder[1] + right_shoulder[1]) / 2]
-        
-        # Calculate angles
-        shoulder_angle = 180 - calculate_angle(left_shoulder, nose, right_shoulder)
-        neck_angle = calculate_angle(mid_shoulder, nose, [nose[0], 0])
-        # shoulder_angle = calculate_angle(left_shoulder, nose, right_shoulder)
-        # neck_angle = calculate_angle(mid_shoulder, nose, [nose[0], mid_shoulder[1]])
-
-        # setting up baseline angles from first 25 frames
-        if not posture_setup_complete and setup_frames < 25:
-            initial_shoulder_angles.append(shoulder_angle)
-            initial_neck_angles.append(neck_angle)
-            setup_frames += 1
-            cv2.putText(frame, f"Gathering data... {setup_frames}/25", 
-                        (10, 30), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
-        
-        # after 25 frames, take the average angles and set thresholds for 
-        # angles
-        elif not posture_setup_complete:
-            shoulder_threshold = np.mean(initial_shoulder_angles) - 10
-            neck_threshold = np.mean(initial_neck_angles) - 10
-            posture_setup_complete = True
-            os.system('clear')           
-            cv2.putText(frame, 
-            f"Setup complete! Shoulder threshold: {shoulder_threshold:.1f} and neck threshold: {neck_threshold:.1f}",
-            (70,80), font, 1, (0,0,0), 2, cv2.LINE_AA)
-            #print(f"Setup complete! Shoulder threshold: {shoulder_threshold:.1f} and neck threshold: {neck_threshold:.1f}")
-
-        # Begin posture detection
-        elif posture_setup_complete:
-            # poor shoulder posture
-            if shoulder_threshold > shoulder_angle and neck_angle > neck_threshold:
-                cv2.putText(frame,
-                f"Poor shoulder posture detected! Please sit up straight. {shoulder_angle:.1f}/{shoulder_threshold:.1f}",
-                (100,50), font, 1, (255,0,0), 2, cv2.LINE_4)
-                poor_posture_detected()
-            
-            # poor neck posture
-            if neck_threshold > neck_angle and shoulder_angle > shoulder_threshold:
-                cv2.putText(frame,
-                f"Poor neck posture detected! Please sit up straight. {neck_angle:.1f}/{neck_threshold:.1f}",
-                (100,50), font, 1, (255,0,0), 2, cv2.LINE_4)
-                poor_posture_detected()
-
-            if shoulder_threshold > shoulder_angle and neck_threshold > neck_angle:
-                cv2.putText(frame,
-                f"""Poor neck and shoulder posture detected! Please sit up straight. Shoulder: {shoulder_angle:.1f}/{shoulder_threshold:.1f} Neck: {neck_angle:.1f}/{neck_threshold:.1f}""",
-                (100,50), font, 1, (255,0,0), 2, cv2.LINE_4)
-                poor_posture_detected()
-            print (nose, left_shoulder, right_shoulder)
-            
-    else: 
-        if (time.time() - (start_time + time_diff)) > 15:
-            start_time = time.time()
 
             # if person not sitting (not in frame)
         
